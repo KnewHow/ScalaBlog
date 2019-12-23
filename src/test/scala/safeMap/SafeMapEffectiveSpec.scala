91a7275e068e9e29daab2d9579a4a5edbb2620f4
KnewHow <948170910@qq.com>
2019-12-23 22:08:57 +0800

    fix

--- src/test/scala/safeMap/SafeMapEffectiveSpec.scala
+++ src/test/scala/safeMap/SafeMapEffectiveSpec.scala
@@ -14,15 +14,22 @@ class SafeMapEffectiveSpec extends FlatSpec {
   "test safe map put and get" should "success" in {
     implicit val sio = Sync[IO].delay(println("Hello world!"))
     val safeMap = SafeMap.empty[IO,Int,Int]
-    val currentMap = new  ConcurrentHashMap[String,String]()
+    
     val seq = Seq.fill(1000)("how")
-    val r1 = seq.map{
-      r => IO.pure(currentMap.putIfAbsent(r,r))
-    }.toVector.parSequence
-    val r2 = vector.map{r =>
-      map.putIfAbsent(key, value)
-    }.toVector.parSequence
-    r1.unsafeRunSync
+
+    def testCurrentMap[F[_]]()(implicit F: ConcurrentEffect[F],P: Parallel[F]) {
+      val currentMap = new  ConcurrentHashMap[String,String]()
+      val a = Seq.iterate(0,1000)(_ + 1).map(_.toString).map(
+         r => IO.pure(currentMap.putIfAbsent(r,r))
+      ).toVector.parSequence
+    }
+    // val r1 = seq.map{
+    //   r => IO.pure(currentMap.putIfAbsent(r,r))
+    // }.toVector.parSequence
+    // val r2 = vector.map{r =>
+    //   map.putIfAbsent(key, value)
+    // }.toVector.parSequence
+    // r1.unsafeRunSync
     succeed
   }
 }
