984a4ab48e717be2c1ed6a9d024ca3a41a7c0e29
how <how@howdeiMac.local>
2019-12-26 09:54:29 +0800

    fix

--- src/test/scala/safeMap/SafeMapEffectiveSpec.scala
+++ src/test/scala/safeMap/SafeMapEffectiveSpec.scala
@@ -8,21 +8,24 @@ import cats.syntax.all._
 import java.util.concurrent._
 import scala.concurrent.ExecutionContext
 import org.scalatest.FlatSpec
+import cats.effect.concurrent.Ref
 
 class SafeMapEffectiveSpec extends FlatSpec {
   "test safe map put and get" should "success" in {
     implicit val sio = Sync[IO].delay(println("Hello world!"))
-    val safeMap = SafeMap.empty[IO,String,String]
+    val safeMap:IO[SafeMap[IO,String,String]] = SafeMap.empty[IO,String,String]
     val ex = Executors.newFixedThreadPool(10);
     implicit val ctx = IO.contextShift(ExecutionContext.fromExecutor(ex))
     val seq = Seq.iterate(0,1000000)(_ + 1).map(_.toString)
     val currentMap = new ConcurrentHashMap[String,String]()
-    val r1 = seq.map(
-         r => IO.pure(currentMap.putIfAbsent("how",r))
-    ).toVector.parSequence
-    val r2 = seq.map{r =>
-      safeMap.flatMap(m => m.putIfAbsent("how", r))
+    val r1 = seq.map{r =>
+      IO.delay(currentMap.putIfAbsent("how",r))
     }.toVector.parSequence
+    val r2 = safeMap.flatMap{map =>
+      seq.map{r =>
+        map.putIfAbsent("how", r)
+      }.toVector.parSequence
+    }
     val b1 = System.currentTimeMillis
     r1.unsafeRunSync
     val b2 = System.currentTimeMillis
